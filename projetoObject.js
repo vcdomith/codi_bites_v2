dcdf8edceb3b07ccb2af624cdf52ae008d9d7b81
Vitor Domith <vcdomith@hotmail.com>
2023-10-02 18:13:32 -0300

    Objeto ElementArray

---
+++ projetoObject.js
@@ -0,0 +1,20 @@
+class Projeto {
+    nome
+    descricao
+    cor
+    tag
+    codigo
+    fotoAutor
+    nomeAutor
+
+    constructor(pNome, pDescricao, pCor, pTag, pCodigo, pFotoAutor, pNomeAutor) {
+        this.nome = pNome
+        this.descricao = pDescricao
+        this.cor = pCor
+        this.tag = pTag
+        this.codigo = pCodigo
+        this.fotoAutor = pFotoAutor 
+        this.nomeAutor = pNomeAutor
+    }
+}
+
--- script.js
+++ script.js
@@ -50,3 +50,47 @@ colorPicker.addEventListener('input', () => {
 // $(document).on('change', 'input[type=color]', function() {
 //     this.parentNode.style.backgroundColor = this.value;
 //   });
+
+// Array de uma nodeList com todos elementos que devem ser salvos de um projeto
+//const elementList = Array.from(document.querySelectorAll('.param-objeto'))
+
+//  const getElementListValues = (list) => list.map((element) => element.value);
+
+class ElementArray {
+
+    constructor(){
+        this.elements = Array.from(document.querySelectorAll('.param-objeto'))
+    }
+
+    getValues(){
+        return this.elements.map((element) => element.value)
+    }
+}
+
+class Projeto {
+    nome
+    descricao
+    cor
+    tag
+    codigo
+    // fotoAutor
+    // nomeAutor
+
+    constructor(pNome, pDescricao, pCor, pTag, pCodigo, pFotoAutor, pNomeAutor) {
+        this.nome = pNome
+        this.descricao = pDescricao
+        this.cor = pCor
+        this.tag = pTag
+        this.codigo = pCodigo
+        // this.fotoAutor = pFotoAutor 
+        // this.nomeAutor = pNomeAutor
+    }
+}
+
+// Ao dar submit
+
+// // Cria um objeto da lista de parametros
+// const listaParametros = new ElementArray()
+
+// // Uso o spread operator para criar o objeto Projeto com base na lista de elementos
+// const projeto = new Projeto(...listaParametros.getValues())
