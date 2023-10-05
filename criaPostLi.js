a7c2f863cb10d4b4031968bfcb8c39739c770149
Vitor Domith <vcdomith@hotmail.com>
2023-10-05 16:55:10 -0300

    Css fix e tentativa ajuste code-input comunidade

---
+++ criaPostLi.js
@@ -0,0 +1,42 @@
+{/* <li class="post">
+    <div class="editor">
+        <div class="opcoes">
+            <div class="icone-wrapper">
+                <div class="quadrado-icone"></div>
+                <div class="quadrado-icone"></div>
+                <div class="quadrado-icone"></div>
+            </div>
+            <h5>Tag</h5>
+        </div>
+        <code-input class="comunidade-codigo" lang="py" placeholder="Escreva seu código aqui"></code-input>
+    </div>
+    <div class="detalhes-post">
+        <textarea name="" id="" class="comunidade-titulo"
+        rows="2">Pesquisa Binária em Python</textarea>
+        <textarea name="" id="" class="comunidade-descricao" rows="3" spellcheck="false" readonly>Descricao projeto asdfasjdçflakjsçdlkfjaçlsdkjfaçlskdj façlskdjfça lksdjf çalskdjf açlskjfçalksdjfaçl skdjfaç laçslkdjfçalskdjf çalskj</textarea>
+        <h4>Python</h4>
+        <div class="social-autor">
+            <div class="social">
+                <div>
+                    <img src="./assets/comentarios.svg" alt=""><p style="margin: 0;">0</p>
+                </div>
+                <div>
+                    <img src="./assets/likes.svg" alt=""><p style="margin: 0;">0</p>
+                </div>
+            </div>
+            <div class="autor">
+                <div>
+                    <img src="./assets/Photo.svg" alt="Foto usuário"><p style="margin: 0;">@Nilvo</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</li> */}
+
+const listaPosts = document.querySelector('.lista-posts')
+
+function criaPost() {
+
+    const liPost = document.createElement('li')
+    
+}
--- textareaReadonlyComunidade.js
+++ textareaReadonlyComunidade.js
@@ -1,4 +1,8 @@
-const editoresCodigo = document.querySelectorAll(textarea)
 
-editoresCodigo.forEach((editor) => editor.setAttribute('readonly', 'true'))
+// // Funcionalidade para o code-input não ser editável apenas na página da comunidade
+// const editoresCodigo = document.querySelectorAll('textarea')
 
+// editoresCodigo.forEach((editor) => editor.setAttribute('readonly', 'true'))
+
+let codeInput = document.querySelector("code-input"); // Or get your element in another way
+codeInput.setAttribute("disabled"); // Make read-only - this currently works.
