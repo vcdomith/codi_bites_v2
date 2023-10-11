28458c07c1f83cc218220e8786804650caf34b92
Vitor Domith <vcdomith@hotmail.com>
2023-10-11 18:14:59 -0300

    Logica de criar elemento post e localstorage

--- criaPostLi.js
+++ criaPostLi.js
@@ -33,6 +33,10 @@
     </div>
 </li> */}
 
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function range(size, startAt = 0) {
     return [...Array(size).keys()].map(i => i + startAt);
 }
@@ -51,7 +55,11 @@ function createNewElement(elementTag, elementClass, elementContent = '') {
     if (elementClass) {
         element.classList.add(elementClass)
     }
-    element.innerText = elementContent
+
+    if (elementContent !== undefined) {
+        element.textContent = elementContent;
+    }
+    // element.innerText = elementContent
 
     return element
 }
@@ -60,129 +68,148 @@ const listaPosts = document.querySelector('.lista-posts')
 
 function criaPost(projeto) {
 
-    // 1|<li class="post">
-    const liPost = createNewElement('li','post')
+    /* LEGENDA:
+        <#_nomeElemento: Abre/Cria o elemento pai
+        #_nomeElemento>: Fechao elemnto pai, dando append em todos os filhos 
+        *#_nomeElemento: Abre/Fecha o elemento de uma vez | Elemento que é apenas filho |
+    */
 
-        //  2|<div class="editor">
+    //<1 <li class="post">
+    const liPost = createNewElement('a','post')
+    liPost.setAttribute('style', 'cursor: pointer;')
+    liPost.setAttribute('style', 'text-decoration: none;')
+    liPost.setAttribute('href', './index.html')
+
+        //<2  <div class="editor">
         const divWrapperEditor = createNewElement('div', 'editor')
 
-            //   3|<div class="opcoes">
+            //<3_wrapperOpcoes   <div class="opcoes">
             const divWrapperOpcoes = createNewElement('div', 'opcoes')
-            //
-                //    4|<div class="icone-wrapper">
+            divWrapperOpcoes.style.backgroundColor = projeto.cor
+            
+                //<4_wrapperIcones    <div class="icone-wrapper">
                 const divWrapperIcones = createNewElement('div', 'icone-wrapper')
 
-                //      5|<div class="quadrado-icone"></div>
-                const divQuadradoIcone = createNewElement('div', 'quadrado-icone')
+                    //*5_quadradoIcones      <div class="quadrado-icone"></div>
+                    const divQuadradoIcone = createNewElement('div', 'quadrado-icone')
                 
-                //    /4|</div class="icone-wrapper">
+                //4_wrapperIcones>    </div class="icone-wrapper">
                 appendMultipleChildren(3, divWrapperIcones, divQuadradoIcone)
             
-                //      5|<h5>Tag</h5>
-                const h5TagPost = createNewElement('h5', elementContent = projeto.tag)
+                //*4      <h5>Tag</h5>
+                const h5TagPost = createNewElement('h5', null, projeto.tag)
               
-            //    /3|<div class="opcoes">
+            //3_wrapperOpcoes>    <div class="opcoes">
+            divWrapperOpcoes.appendChild(divWrapperIcones)
             divWrapperOpcoes.appendChild(h5TagPost)
         
-            //   3|<code-input class="comunidade-codigo" lang="py" placeholder="Escreva seu código aqui"></code-input>
-            const codeInput = createNewElement('code-input','comunidade-codigo')
+            //*3_codeInput   <code-input class="comunidade-codigo" lang="py" placeholder="Escreva seu código aqui"></code-input>
+            const codeInput = createNewElement('code-input','comunidade-codigo', projeto.codigo)
             codeInput.setAttribute('lang', projeto.linguagem)
             codeInput.setAttribute('placeholder', 'Escreva seu código aqui')
+            codeInput.style.pointerEvents="none"
 
-        //  /2|<div class="editor">
-        divWrapperEditor.appendChild(divWrapperEditor)
+        //2_wrapperEditor>  <div class="editor">
+        divWrapperEditor.appendChild(divWrapperOpcoes)
+        divWrapperEditor.appendChild(codeInput)
 
-        //   2|<div class="detalhes-post">
+        //<2_wrapperDetalhes   <div class="detalhes-post">
         const divWrapperDetalhes = createNewElement('div', 'detalhes-post')
 
-            //   3|<textarea name="" id="" class="comunidade-titulo" rows="2">Pesquisa Binária em Python</textarea>
-            const textareaTitulo = createNewElement('textarea', 'comunidade-titulo')
+            //*3_textareaTitulo   <textarea name="" id="" class="comunidade-titulo" rows="2">Pesquisa Binária em Python</textarea>
+            const textareaTitulo = createNewElement('textarea', 'comunidade-titulo', projeto.titulo)
             textareaTitulo.setAttribute('rows', '2')
+            textareaTitulo.style.pointerEvents="none"
 
-            //   3|<textarea name="" id="" class="comunidade-descricao" rows="3" spellcheck="false" readonly></textarea>
-            const textareaDecricao = createNewElement('textarea', 'comunidade-descricao')
-            textareaDecricao.setAttribute('rows', '3')
-            textareaDecricao.setAttribute('spellcheck', 'false')
-            textareaDecricao.setAttribute('readonly', 'true')
+            //*3_textareaDescricao   <textarea name="" id="" class="comunidade-descricao" rows="3" spellcheck="false" readonly></textarea>
+            const textareaDescricao = createNewElement('textarea', 'comunidade-descricao', projeto.descricao)
+            textareaDescricao.setAttribute('rows', '3')
+            textareaDescricao.setAttribute('spellcheck', 'false')
+            textareaDescricao.setAttribute('readonly', 'true')
+            textareaDescricao.style.pointerEvents="none"
 
-            //   3|<h4>Linguagem</h4>
-            const h4Linguagem = createNewElement('h4')
+            //*3_linguagem   <h4>Linguagem</h4>
+            const h4Linguagem = createNewElement('h4', null, capitalize(projeto.linguagem))
 
-            //   3|<div class="social-autor">
+            //<3_wrapperSocialAutor   <div class="social-autor">
             const divWrapperSocialAutor = createNewElement('div', 'social-autor')
 
-                //    4|<div class="social">
+                //<4_wrapperSocial    <div class="social">
                 const divWrapperSocial = createNewElement('div', 'social')
 
-                    //     5|<div>
+                    //<5_wrapperComentarios     <div>
                     const divWrapperComentarios = createNewElement('div')
 
-                        //      6|<img src="./assets/comentarios.svg" alt="imagem comentarios">
+                        //*6_imgComentarios      <img src="./assets/comentarios.svg" alt="imagem comentarios">
                         const imgComentarios = createNewElement('img')
                         imgComentarios.setAttribute('src', './assets/comentarios.svg')
                         imgComentarios.setAttribute('alt', 'imagem comentarios')
 
-                        //      6|<p style="margin: 0;">0</p>
-                        const pQuantidadeComentarios = createNewElement('p', elementContent = projeto.comentarios)
+                        //*6_pQuantComentarios      <p style="margin: 0;">0</p>
+                        const pQuantidadeComentarios = createNewElement('p', null, projeto.comentarios)
                         pQuantidadeComentarios.setAttribute('style', 'margin: 0;')
 
-                    //     /5|</div>
+                    //5_wrapperComentarios>     </div>
                     divWrapperComentarios.appendChild(imgComentarios)
                     divWrapperComentarios.appendChild(pQuantidadeComentarios)
 
-                    //    5|<div>
+                    //<5_wrapperLikes    <div>
                     const divWrapperLikes = createNewElement('div')
 
-                        //      6|<img src="./assets/likes.svg" alt="imagem coração likes">
+                        //*6_imgLikes      <img src="./assets/likes.svg" alt="imagem coração likes">
                         const imgLikes = createNewElement('img')
                         imgLikes.setAttribute('src', './assets/likes.svg')
                         imgLikes.setAttribute('alt', 'imagem coração likes')
 
-                        //      6|<p style="margin: 0;">0</p>
-                        const pQuantidadeLikes = createNewElement('p')
+                        //*6_pQuantLikes      <p style="margin: 0;">0</p>
+                        const pQuantidadeLikes = createNewElement('p', null, projeto.likes)
                         pQuantidadeLikes.setAttribute('style', 'margin: 0;')
 
-                    //     /5|</div>
+                    //5_wrapperLikes>     </div>
                     divWrapperLikes.appendChild(imgLikes)
                     divWrapperLikes.appendChild(pQuantidadeLikes)
 
-                //    /4|</div class="social">
+                //4_wrapperSocial>    </div class="social">
                 divWrapperSocial.appendChild(divWrapperComentarios)
                 divWrapperSocial.appendChild(divWrapperLikes)
 
-                //    4|<div class="autor">
+                //<4_outerAutor    <div class="autor">
                 const divAutor = createNewElement('div', 'autor')
 
-                    //     5|<div>
+                    //<5_innerAutor     <div>
                     const divWrapperAutor = createNewElement('div')
 
-                        //      6|<img src="./assets/Photo.svg" alt="Foto usuário">
+                        //*6_fotoAutor      <img src="./assets/Photo.svg" alt="Foto usuário">
                         const imgAutor = createNewElement('img')
                         imgAutor.setAttribute('src', projeto.fotoAutor)
                         imgAutor.setAttribute('alt', 'Foto usuário')
 
-                        //      6|<p style="margin: 0;">@Nome Autor</p>
-                        const pAutor = createNewElement('p', projeto.nomeAutor)
+                        //*6_nomeAutor      <p style="margin: 0;">@Nome Autor</p>
+                        const pAutor = createNewElement('p', null, projeto.nomeAutor)
                         pAutor.setAttribute('style', 'margin: 0;')
                     
-                    //     /5|</div>
+                    //5_innerAutor>     </div>
                     divWrapperAutor.appendChild(imgAutor)
                     divWrapperAutor.appendChild(pAutor)
 
-                //    /4|</div class="autor">
+                //4_outerAutor>    </div class="autor">
                 divAutor.appendChild(divWrapperAutor)
             
-            //   /3|</div class="social-autor">
+            //3_wrapperSocialAutor>   </div class="social-autor">
             divWrapperSocialAutor.appendChild(divWrapperSocial)
             divWrapperSocialAutor.appendChild(divAutor)
 
-        //  /2|<div class="detalhes-post">
-        divWrapperEditor.appendChild(codeInput)
-
-    //    
-    // /1|<li class="post">
+        //2_wrapperDetalhes>  <div class="detalhes-post">
+        divWrapperDetalhes.appendChild(textareaTitulo)
+        divWrapperDetalhes.appendChild(textareaDescricao)
+        divWrapperDetalhes.appendChild(h4Linguagem)
+        divWrapperDetalhes.appendChild(divWrapperSocialAutor)
+  
+    //1_liPost> <li class="post">
     liPost.appendChild(divWrapperEditor)
     liPost.appendChild(divWrapperDetalhes)
+
+    listaPosts.appendChild(liPost)
 }
 
 /* 
@@ -198,3 +225,20 @@ Guardado aqui caso o appendMultipleChildren() não funcione
 //     //    /4|<div class="icone-wrapper">
 //     divWrapperIcones.appendChild(divQuadradoIcone)
 // }
+
+/*
+
+projeto.tag
+projeto.codigo
+projeto.linguagem --> string minusculas
+projeto.titulo
+projeto.descricao
+projeto.linguagem --> função que capitaliza
+projeto.comentarios
+projeto.likes
+projeto.fotoAutor
+projeto.nomeAutor
+
+-------| Falta |----------
+
+*/
--- script.js
+++ script.js
@@ -54,6 +54,32 @@ class ElementArray {
     getValues(){
         return this.elements.map((element) => element.value)
     }
+
+    getKeys(){
+        return this.elements.map((element) => element.classList[0])
+    }
+}
+
+function criaProjeto(ElementArray) {
+
+    const tagText = document.querySelector('.tag')
+
+    const keys = ElementArray.getKeys()
+    const values = ElementArray.getValues()
+
+    const projeto = {
+        fotoAutor: './assets/Photo.svg',
+        nomeAutor: 'Nilvo',
+        comentarios: 0,
+        likes: 0,
+        corTexto: tagText.style.color
+    }
+
+    keys.forEach((element, index) => {
+        projeto[element] = values[index]
+    })
+
+    return projeto
 }
 
 class Projeto {
@@ -83,3 +109,28 @@ class Projeto {
 
 // // Uso o spread operator para criar o objeto Projeto com base na lista de elementos
 // const projeto = new Projeto(...listaParametros.getValues())
+
+function salvaProjeto(array) {
+
+    const projeto = array.elements.map()
+
+}
+
+
+
+/*
+
+Lógica para guardar e depois criar lista com todos os posts
+
+const a = new ElementArray()
+const projeto = criaProjeto(a)
+
+localStorage.setItem(`${projeto.titulo}`, JSON.stringify(projeto))
+
+-> Lista com todas as keys(projetos) no localStorage 
+const listaProjetos = Object.keys(localStorage)
+
+-> criaPost() para cada projeto dentro do localStorage
+listaProjetos.forEach((key) => criaPost(JSON.parse(localStorage[key])))
+
+*/
