2c6ae3b22ea906cb96920d4c2e2ee6501f821bfa
Vitor Domith <vcdomith@hotmail.com>
2023-11-24 18:11:14 -0300

    CSS Novo Projeto

--- scripts/github_api/githubs_access.js
+++ scripts/github_api/githubs_access.js
@@ -1,9 +1,6 @@
 
 
-async function acessaArquivo() {
-
-    // Personal Acess Token
-    const token = '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'
+async function acessaArquivo(token) {
 
     // Personal Repository data
     const owner = 'vcdomith'
@@ -35,6 +32,36 @@ async function acessaArquivo() {
 
     }
 
-    const erro = await api
+}
+
+async function acessaRepo() {
+
+    // Personal Repository data
+    const owner = 'vcdomith'
+    const repo = 'codi_bites'
+    const path = '_projetos/'
+
+    // GitHub API endpoint to get the contents of a repository's path
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+
+    try {
+
+        const response = await fetch(apiUrl)
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error. Status: ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        console.log(data)
+
+    }   catch(error) {
+
+        console.error(error)
+
+    }
+
+
 
 }
