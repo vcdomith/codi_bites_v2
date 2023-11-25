c36a890f2a06b14671e8e9b9e202aac52b279f7b
Vitor Domith <vcdomith@hotmail.com>
2023-11-25 00:22:15 -0300

    Repo access

---
+++ scripts/db_handler/google_sheets.js
@@ -0,0 +1,18 @@
+
+// const url = 'https://docs.google.com/spreadsheets/d/1umri1KVIY8TWdn1XZpX1bR2dNb02DOj0iw6WQTY_1-Q/edit?usp=sharing'
+
+// const id = '1umri1KVIY8TWdn1XZpX1bR2dNb02DOj0iw6WQTY_1-Q'
+
+// console.log('google-sheetsjs');
+
+// window.addEventListener('beforeunload', function(event) {
+
+//     event.preventDefault()
+
+//     const confirm = confirm('Se você sair seus dados não salvos serão perdidos. Confirma saída:')
+
+//     if (confirm) {
+//         this.alert('aaaaaaaaaaaaaaaa');
+//     }
+
+// })
---
+++ scripts/db_handler/leitor_pasta_projeto.js
@@ -0,0 +1,57 @@
+
+function getUrlAtual() {
+
+    const urlAtual = window.location.href
+    return urlAtual
+    
+
+}
+
+
+async function acessaRepo() {
+
+    const url = getUrlAtual()
+    
+    // Get Repository url data
+    let owner, repo, path
+
+    if (url.includes('github')) {
+ 
+        const urlTratado = new URL(url)
+
+        owner = urlTratado.hostname.slice(0, urlTratado.hostname.indexOf('.'))
+        repo = urlTratado.pathname.replaceAll('/', '')
+        path = '_projetos/'
+        
+    
+    } else {
+    
+        owner = 'vcdomith'
+        repo = 'codi_bites'
+        path = '_projetos/'
+        
+    }
+
+    // GitHub API endpoint to get the contents of a repository's path
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+
+    try {
+
+        const response = await fetch(apiUrl)
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error. Status: ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        console.log(data)
+
+    }   catch(error) {
+
+        console.error(error)
+
+    }
+
+}
+acessaRepo()
